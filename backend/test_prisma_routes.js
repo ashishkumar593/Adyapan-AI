45dc0bf4b1c7c9414d5e3cda7882ae459b72e637
ashishkumar593 <ashishjat593@gmail.com>
2026-07-04 01:16:34 +0530

    chore: remove debug/test files, update env to production URLs

--- backend/test_prisma_routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { prisma } = require('./dist/config/prisma');
-const { apiRouter } = require('./dist/routes');
-const app = express();
-app.use('/api', apiRouter);
-app.listen(5003, () => {
-  console.log('Express with Prisma and Routes running on 5003');
-});
--- backend/test_real_index_port.js
+++
@@ -1,5 +0,0 @@
-process.env.PORT = '5003';
-process.on('exit', (code) => {
-  console.log('PROCESS EXIT EVENT WITH CODE:', code);
-});
-require('./dist/index.js');
